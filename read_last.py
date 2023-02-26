14db6a6ad6a71599824490b2a119455bd2f80f06
Vanger <stepan13@gmail.com>
2023-02-26 18:39:30 +0000

    initial commit

---
+++ read_last.py
@@ -0,0 +1,36 @@
+def read_last(lines_count, filename):
+    if lines_count <= 0:
+        return
+    with open(filename,"r",encoding="UTF-8") as file:
+        lines = file.readlines()
+        lines = list(map(lambda x: x.strip(),lines))
+        all_lines = len(lines)
+        lines_count = min(lines_count,all_lines)
+        for i in range(lines_count):
+            print(lines[all_lines-lines_count+i])
+            # print(lines[-i-1]) # выведет в обратном порядке
+
+def longest_words(filename):
+    res = list()
+    max_len = 0
+    with open(filename,"r",encoding="UTF-8") as file:
+        lines = file.read().splitlines()
+        # lines = list(map(lambda x: x.strip(),lines))
+        for line in lines:
+            words = line.split(" ")
+            for word in words:
+                current_Len = len(word)
+                if current_Len > max_len:
+                    res.clear()
+                    res.append(word)
+                    max_len = current_Len
+                elif current_Len == max_len:
+                    res.append(word)
+    print("Самые длинные слова: ", *res)
+                    
+
+
+
+input_lines = int(input("ВВедите количество строк: "))
+read_last(input_lines,"article.txt")
+longest_words("article.txt")
